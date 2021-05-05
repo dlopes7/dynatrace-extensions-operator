edd6ac6b97bf630736c1667a7986715c1fb270ee
dlopes7 <davidribeirolopes@gmail.com>
2021-05-05 14:07:06 -0300

    Add daemonset implementation

--- controllers/extension_controller.go
+++ controllers/extension_controller.go
@@ -18,7 +18,13 @@ package controllers
 
 import (
 	"context"
+	"hash/fnv"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/json"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"strconv"
 
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
@@ -29,6 +35,8 @@ import (
 
 	dynatracev1alpha1 "quay.io/dlopes7/dt-extensions-operator/api/v1alpha1"
 )
+
+const AnnotationTemplateHash = "internal.extension.dynatrace.com/template-hash"
 
 // ExtensionReconciler reconciles a Extension object
 type ExtensionReconciler struct {
@@ -63,7 +71,134 @@ func (r *ExtensionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	r.Log.Info("Found instance", "instance", instance)
 
+	dsDesired, err := newDaemonSet(instance)
+	if err != nil {
+		r.Log.Info("Failed to get desired daemonset")
+		return reconcile.Result{}, err
+	}
+
+	dsActual := &appsv1.DaemonSet{}
+	err = r.Client.Get(ctx, types.NamespacedName{Name: dsDesired.Name, Namespace: dsDesired.Namespace}, dsActual)
+	if err != nil && k8serrors.IsNotFound(err) {
+		r.Log.Info("Creating new daemonset")
+		if err = r.Client.Create(ctx, dsDesired); err != nil {
+			return reconcile.Result{}, err
+		}
+	} else if err != nil {
+		return reconcile.Result{}, err
+	} else if hasDaemonSetChanged(dsDesired, dsActual) {
+		r.Log.Info("Updating existing daemonset")
+		if err = r.Client.Update(ctx, dsDesired); err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+
 	return ctrl.Result{}, nil
+}
+
+func getTemplateHash(a metav1.Object) string {
+	if annotations := a.GetAnnotations(); annotations != nil {
+		return annotations[AnnotationTemplateHash]
+	}
+	return ""
+}
+
+func hasDaemonSetChanged(a, b *appsv1.DaemonSet) bool {
+	return getTemplateHash(a) != getTemplateHash(b)
+}
+
+func newPodSpec() corev1.PodSpec {
+
+	p := corev1.PodSpec{
+		Volumes: []corev1.Volume{
+			{
+				Name: "host-root",
+				VolumeSource: corev1.VolumeSource{
+					HostPath: &corev1.HostPathVolumeSource{
+						Path: "/",
+					},
+				},
+			},
+		},
+		Containers: []corev1.Container{{
+			Image:           "quay.io/dlopes7/dt-extension:latest",
+			ImagePullPolicy: corev1.PullAlways,
+			Name:            "dt-extension",
+			ReadinessProbe: &corev1.Probe{
+				Handler: corev1.Handler{
+					Exec: &corev1.ExecAction{
+						Command: []string{
+							"/extension-watcher", "health-check",
+						},
+					},
+				},
+				InitialDelaySeconds: 30,
+				PeriodSeconds:       30,
+				TimeoutSeconds:      1,
+			},
+			// SecurityContext: secCtx,
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "host-root",
+					MountPath: "/mnt/root",
+				},
+			}},
+		},
+	}
+
+	return p
+}
+
+func newDaemonSet(instance *dynatracev1alpha1.Extension) (*appsv1.DaemonSet, error) {
+
+	podSpec := newPodSpec()
+
+	selectorLabels := buildLabels(instance.GetName())
+	ds := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        instance.GetName(),
+			Labels:      selectorLabels,
+			Namespace:   instance.GetNamespace(),
+			Annotations: map[string]string{},
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{MatchLabels: selectorLabels},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: selectorLabels,
+				},
+				Spec: podSpec,
+			},
+		},
+	}
+	dsHash, err := generateDaemonSetHash(ds)
+	if err != nil {
+		return nil, err
+	}
+	ds.Annotations[AnnotationTemplateHash] = dsHash
+
+	return ds, nil
+}
+
+func buildLabels(name string) map[string]string {
+	return map[string]string{
+		"dynatrace.com/component":          "extension",
+		"extension.dynatrace.com/instance": name,
+	}
+}
+func generateDaemonSetHash(ds *appsv1.DaemonSet) (string, error) {
+	data, err := json.Marshal(ds)
+	if err != nil {
+		return "", err
+	}
+
+	hasher := fnv.New32()
+	_, err = hasher.Write(data)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.FormatUint(uint64(hasher.Sum32()), 10), nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
